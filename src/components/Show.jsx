b20d3e926f42c8a24416c4a8ab6dd4a485f71612
Ileana Aguilar <73076457+ileana-aguilar@users.noreply.github.com>
2024-02-29 18:56:03 -0500

    modified

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Show ({img, name, url}) {
 return(
     <section className="Card">
-        <img src={img} title={name} alt={name}  />
+        <img src={img} title={name} alt={name}/>
         <h5> {name}</h5>
         <a className="Button" href={url}>  Show's Website </a>
     </section>
